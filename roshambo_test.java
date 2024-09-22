53a1a486e31f05512cd59aca75f51e2dc2345a27
an-adventure-awaits <amcintire12345@gmail.com>
2024-09-21 23:05:57 -0600

    Update roshambo_test.java

--- roshambo_test.java
+++ roshambo_test.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
     
-public class test{
+public class roshambo_test{
     public static void main(String[] args) {
         
         Scanner keyboard = new Scanner(System.in);
