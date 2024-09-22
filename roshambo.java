048af9b12629db32e31173b1eb2d2983689bb4c8
an-adventure-awaits <amcintire12345@gmail.com>
2024-09-21 23:04:32 -0600

    testing roshambo
    fixing bugs

---
+++ roshambo.java
@@ -0,0 +1,81 @@
+// Name: roshambo.java
+// Author: Ansonia McIntire
+// Date: 9/21/2024
+// Purpose: roshambo
+
+import java.util.Scanner;
+
+    
+public class roshambo{
+    public static void main(String[] args) {
+        
+        Scanner keyboard = new Scanner(System.in);
+        
+            int computer;
+            int user;
+            int user_input;
+            int rock;
+            int paper;
+            int scissors;
+            
+            //title
+            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            System.out.println("~  THE EPIC ROSHAMBO!!!  ~");
+            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+            System.out.println("\n Rock = 1, \n Paper = 2, \n Scissors =3");
+            System.out.print("What is your number? ");
+
+            computer += 1;
+            user += 1;
+            rock = 1;
+            paper = 2;
+            scissors = 3;
+
+
+
+            if computer == "rock";
+
+                if user == "rock";
+                System.out.println("You picked rock!");
+
+                else if == "paper";
+                System.out.println("You picked paper!");
+
+                else if == "scissors";
+                System.out.println("You picked scissors!");
+
+                
+            
+            else if computer == "paper";
+
+                if user == "rock";
+                System.out.println("You picked rock!");
+
+                else if == "paper";
+                System.out.println("You picked paper!");
+
+                else if == "scissors";
+                System.out.println("You picked scissors!");
+                
+
+
+            else if computer == "scissors";
+
+                if user == "rock";
+                System.out.println("You picked rock!");
+
+                else if == "paper";
+                System.out.println("You picked paper!");
+
+                else if == "scissors";
+                System.out.println("You picked scissors!");
+
+
+
+        keyboard.close();
+
+    }
+}
+
+    
---
+++ roshambo_test.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+    
+public class test{
+    public static void main(String[] args) {
+        
+        Scanner keyboard = new Scanner(System.in);
+
+
+//title
+System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+System.out.println("~  THE EPIC ROSHAMBO!!!  ~");
+System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+System.out.println("\n Rock = 1, \n Paper = 2, \n Scissors =3");
+System.out.print("What is your number? ");
+
+    }}
