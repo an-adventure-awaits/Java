89f878b36233d5beac84c75c6b9332a3d4811478
an-adventure-awaits <amcintire12345@gmail.com>
2024-09-25 16:10:41 -0600

    while loop

--- roshambo.java
+++ roshambo.java
@@ -25,69 +25,76 @@ public class roshambo{
             int scissors = 3;
             int score_computer =+ 1;
             int score_user =+ 1;
+            int running_total = 0;
+            int still_running = 4;
+            int computer_win = 0;
+            int player_win = 0;
+
             
             //title
             System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~");
             System.out.println("~  THE EPIC ROSHAMBO!!!  ~");
             System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-            System.out.println("\n Rock = 1, \n Paper = 2, \n Scissors = 3,");
+            //loop
+            while (still_running == 4) {
+                System.out.println("\n Rock = 1, \n Paper = 2, \n Scissors = 3,");
 
-            
-            System.out.print("What number do you pick? ");
-            int user = keyboard.nextInt();
-
-
-            
-            // if 
-            if (computer == rock) {
-
-                if (user == rock) {
-                System.out.println("You picked rock!");
-                }
-
-                else if (user == paper) {
-                System.out.println("You picked paper!");
-                }
-
-                else if (user == scissors) {
-                System.out.println("You picked scissors!");
-                }
-                }
-
-            
-            // else if 
-            else if (computer == paper) {
-
-                if (user == rock) {
-                System.out.println("You picked rock!");
-                }
-
-                else if (user == paper) {
-                System.out.println("You picked paper!");
-                }
-
-                else if (user == scissors) {
-                System.out.println("You picked scissors!");
-                }}
                 
+                System.out.print("What number do you pick? ");
+                int user = keyboard.nextInt();
 
-            // else if 
-            else if (computer == scissors) {
 
-                if (user == rock) {
-                System.out.println("You picked rock!");
-                }
+                // if 
+                if (computer == rock) {
 
-                else if (user == paper) {
-                System.out.println("You picked paper!");
-                }
+                    if (user == rock) {
 
-                else if (user == scissors) {
-                System.out.println("You picked scissors!");
-                }
-                }
+                    System.out.println("You picked rock!");
+                    }
+
+                    else if (user == paper) {
+                    System.out.println("You picked paper!");
+                    }
+
+                    else if (user == scissors) {
+                    System.out.println("You picked scissors!");
+                    }
+                    }
+
                 
+                // else if 
+                else if (computer == paper) {
+
+                    if (user == rock) {
+                    System.out.println("You picked rock!");
+                    }
+
+                    else if (user == paper) {
+                    System.out.println("You picked paper!");
+                    }
+
+                    else if (user == scissors) {
+                    System.out.println("You picked scissors!");
+                    }}
+                    
+
+                // else if 
+                else if (computer == scissors) {
+
+                    if (user == rock) {
+                    System.out.println("You picked rock!");
+                    }
+
+                    else if (user == paper) {
+                    System.out.println("You picked paper!");
+                    }
+
+                    else if (user == scissors) {
+                    System.out.println("You picked scissors!");
+                    }
+                    }}
+                    
 
         // close out program
         keyboard.close();
